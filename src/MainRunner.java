ab55099b4fe4c066c90193e4f413d3dd2d4b947e
Imran Fazilov <ifazilov3584@bths.edu>
2023-01-10 12:24:48 -0500

    period end

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class MainRunner {
+public class MainRunner {
+    public static void main(String[] args) {
+        Person p1 = new Person("Adam", 12);
+        Person p2 = new Person("Barb", 15);
+        Person p3 = new Person("Cathy", 14);
+        Person p4 = new Person("Fred", 16);
+
+
+    }
 }
